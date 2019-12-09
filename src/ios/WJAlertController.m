f5a24a06a3602f481be0977ff58c347de3553c78
zl <zl@wjdeiMac.local>
2019-12-09 17:08:28 +0800

    增加提示

--- src/ios/WJAlertController.m
+++ src/ios/WJAlertController.m
@@ -15,7 +15,6 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
-
     if (echo != nil && [echo length] > 0) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
     } else {
